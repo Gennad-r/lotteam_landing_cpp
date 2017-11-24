5409428be3de97f77e7c03aa7b35a0656528c2a1
Gennad-r <gennad.r@gmail.com>
2017-11-24 14:56:11 +0200

    finished

--- app/js/common.js
+++ app/js/common.js
@@ -104,36 +104,29 @@ function lineDraw() {
 
 $(".owl-carousel").owlCarousel({
 	items: 1,
-	nav: true
+	nav: true,
+	loop: true,
+	autoplay: true,
+	autoplayTimeout: 3000,
+	autoplayHoverPause: true
 });
 
 
 //first screen slide
-function slideFirst(cb) {
+function slideFirst() {
 		$('html, body').animate({
 			scrollTop: $('#challenges').offset().top
-		}, 500, function () {
-			//$('#first, video').css({"display":"none"});
-			
-			cb();
-		});
+		}, 500);
 }
 
-function slideFirstScreen() {
-	slideFirst(function () {
-		$('.top-nav').addClass('regular-nav');
-	});
-}
 
-$('.scroll').click(function () {
-	slideFirstScreen();
-});
+$('.scroll').click(slideFirst);
 
 setTimeout(function () {
 	if ($(window).scrollTop() == 0) {
-		slideFirstScreen();
+		slideFirst();
 	}
-}, 100);
+}, 10000);
 
 // icons animation
 Snap.selectAll(".b-icon path").attr({
@@ -142,93 +135,128 @@ Snap.selectAll(".b-icon path").attr({
 	'transform-origin' : '50% 50%'
 });
 
-function BIcon(id) {
-	this.icon = {
-		'obj' : $(id),
-		'strs' : Snap(id).selectAll(".icon-1-f path"),
-		'mObj' : Snap(id).selectAll(".icon-1-b path"),
-		'scrolled' : false,
-		
-	};
-	//this.scrolled = false;
-	this.run = function (srartAttr, finishAttr) {
-		var objPos = this.icon.obj.offset().top,
-			mObj = this.icon.mObj,
-			cScrolled = this.icon.scrolled,
-			strs = this.icon.strs;
-			//scrolled = false;
-		$(window).on('scroll', function () { //start
-			if ($(window).height() + $(window).scrollTop() > objPos) {
-				cScrolled = true;
-				console.log(cScrolled);
-			}
-			if (cScrolled) {
-				//$(window).off();
-				console.log(cScrolled);
-				mObj.attr(srartAttr).animate(finishAttr, 3000, mina.easein);
-				for (var i = 0; i < strs.length; i++) {
-					var str = strs[i],
-					len = Math.ceil(str.getTotalLength());
-					str.attr({
-						'strokeDasharray': len,
-						'strokeDashoffset': len,
-						'stroke-width': 2,
-						'stroke-linecap':'round',
-						'stroke': accentColor,
-						'fill' : 'none'
+function IC(id, param) {
+	this.obj = $(id);
+	this.position = $(id).position().top;
+	this.strs = Snap(id).selectAll(".icon-1-f path");
+	this.mObj = Snap(id).selectAll(".icon-1-b path");
+	this.scrolled = false;
+	this.param = param;
+	this.param.fill = greyColor;
+	this.run = function () {
+		this.scrolled = true;
+		this.mObj.attr(this.param).animate({'transform':'translate(0, 0)'}, 3000, mina.easein);
+		for (var i = 0; i < this.strs.length; i++) {
+			var str = this.strs[i],
+			len = Math.ceil(str.getTotalLength());
+			str.attr({
+				'strokeDasharray': len,
+				'strokeDashoffset': len,
+				'stroke-width': 2,
+				'stroke-linecap':'round',
+				'stroke': accentColor,
+				'fill' : 'none'
+			});
+			(function() {
+				var strClosure = str;
+				Snap.animate(len, 0, function( val ){
+					strClosure.attr({
+						'strokeDashoffset': val,
 					});
-					(function() {
-						var strClosure = str;
-						Snap.animate(len, 0, function( val ){
-							strClosure.attr({
-								'strokeDashoffset': val,
-							});
-						}, 900, mina.easeout, function () {
-							strClosure.attr({
-								'fill' : accentColor,
-								'stroke' : 'none'
-							});
-						});
-					})();
+				}, 900, mina.easeout, function () {
+					strClosure.attr({
+						'fill' : accentColor,
+						'stroke' : 'none'
+					});
+				});
+			})();
 				} //for
-			}
-			return this;
-		}); //finish
-	};
-}
+			};
+		}
 
 
-var icon1 = new BIcon("#icon1").run({
-	'transform':'translate(0,-50)',
-	'fill': greyColor
-}, {
-	'transform':'translate(0,0)'
-});
-var icon2 = new BIcon("#icon2").run({
-	'transform-origin' : '50% 70%',
-	'transform':'rotate(-90)',
-	'fill': greyColor
-}, {
-	'transform':'translate(0,0)'
-});
-var icon3 = new BIcon("#icon3").run({
-	'transform':'scale(0.5)',
-	'fill': greyColor
-}, {
-	'transform':'translate(0,0)'
+var iconCollection = [new IC("#icon1", {'transform':'translate(0, -50)'}),
+						new IC("#icon2", {'transform':'rotate(-50)'}),
+						new IC("#icon3", {'transform':'scale(1.2)'}),
+						new IC("#icon4", {'transform':'translate(100, 0) rotate(80)'}),
+						new IC("#icon5", {'transform':'scale(1.4) rotate(-50)'}),
+						new IC("#aboutUsIcon", {})];
+
+var winHeight = $(window).innerHeight();
+
+$(window).scroll(function () {
+	var top = $(window).scrollTop();
+	if (top >= winHeight - $('.top-nav').height()) {
+		$('.top-nav').addClass('regular-nav');
+	} else {$('.top-nav').removeClass('regular-nav');}
+	for (var i = 0; i < iconCollection.length; i++) {
+		var run = top + winHeight >= iconCollection[i].position;
+		if (!iconCollection[i].scrolled && run){
+			iconCollection[i].run();
+		}
+	}
+	// Assign active class to nav links while scolling
+	$('section').each(function(i) {
+			//console.log($(this).position().top);
+			//console.log($('.navigation a').eq(i));
+		if ($(this).position().top - $('.navigation').height() <= $(window).scrollTop()) {
+
+			$('.navigation a.active').removeClass('active');
+			$('.navigation a').eq(i).addClass('active');
+		}
+	});
+
 });
 
-var icon4 = new BIcon("#icon4").run({
-	'transform':'translate(0,-60) rotate(-120)',
-	'fill': greyColor
-}, {
-	'transform':'translate(0,0)'
+//scroll to
+
+$('.navigation a').bind('click', function(e) {
+	e.preventDefault(); // prevent hard jump, the default behavior
+	$(".navigation").removeClass("clicked");
+	$(".hamburger--elastic").removeClass("is-active");
+	var target = $(this).attr("href") || $(this).attr("data"), // Set the target as variable
+		pos = $(target).offset().top; // navigation panel heigth
+	// perform animated scrolling by getting top-position of target-element and set it as scroll target
+	$('html, body').stop().animate({
+		scrollTop: pos
+	}, 600/*, function() {
+			location.hash = target; //attach the hash (#jumptarget) to the pageurl
+		}*/);
+
+	return false;
 });
 
-var icon5 = new BIcon("#icon5").run({
-	'transform':'scale(1.7) rotate(-90)',
-	'fill': greyColor
-}, {
-	'transform':'translate(0,0)'
-});
 
+// hamburger menu
+	$('.hamburger--elastic').click(function () {
+			$(this).toggleClass("is-active");
+			$(".navigation").toggleClass("clicked");
+	});
+
+
+//E-mail Ajax Send
+
+$("#contact-us").submit(function() { //Change
+		var th = $(this);
+		$.getJSON('//freegeoip.net/json/?callback=?', function(data) {
+			$("input[name='customer-country']").attr('value', data.country_name);
+			$("input[name='customer-ip']").attr('value', data.ip);
+			$.ajax({
+				type: "POST",
+				url: "mail.php", //Change
+				data: th.serialize()
+			}).done(function() {
+				$(".thanks").css("display", "table");
+				$(".thanks").on("click", function () {
+					$(".thanks").hide();
+					$(".thanks").off("click");
+				});
+				$("#contact-us").hide();
+				setTimeout(function() {
+					// Done Functions
+					th.trigger("reset");
+				}, 1000);
+			});
+		});
+	return false;
+});
